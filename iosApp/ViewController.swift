e2bbf7d5ca80d7a51b0f0fc7412d55a5c00da016
Efren Lamolda <efren@efrens-macbook-pro.local>
2018-09-21 10:25:46 +0200

    Initial Commit

---
+++ iosApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosApp
+//
+//  Created by Efren Alvarez Lamolda on 21/09/2018.
+//  Copyright © 2018 Efren Alvarez Lamolda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
